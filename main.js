bb3f4cdd5dca9aac3e6be55fd519bb6e7b34562e
gabrielSeixas <gabriel.seixas@webedia-group.com>
2017-10-26 18:01:43 -0200

    Removed main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-const { ddmmyyISO } = require("./dist/briefly.min");
-
-console.log(ddmmyyISO);
-
-console.log("=============================");
-console.log(ddmmyyISO("2017-10-08T07:00:00.000Z"));
-console.log("=============================");
